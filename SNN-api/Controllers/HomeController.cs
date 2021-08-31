61f1a5f6fa340b738a5541bf0b1fb72ec2b90506
Wael Hallag <info@net3000.ca>
2021-08-31 13:31:12 -0400

    added API application

---
+++ SNN-api/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+
+namespace SNN_api.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        IConfiguration myConfig;
+        public HomeController(IConfiguration config) {
+            myConfig = config;
+        }
+
+        [HttpGet("/")]
+        public string index()
+        {
+            return "Index";
+        }
+
+        [HttpGet("qm/{*queryvalues}")]
+        public string qmIndex()
+        {
+            WebClient myClient = new WebClient();
+            string appendWebmaster = null;
+            if (HttpContext.Request.Query != null && HttpContext.Request.Query.Keys.Count > 0)
+            {
+                appendWebmaster = "&";
+            }
+            else {
+                appendWebmaster = "?";
+            }
+            myClient.Headers.Add("Authorization", $"Bearer {myConfig.GetValue<string>("Quotemedia:token")}");
+            try {
+                var myData = myClient.DownloadString($"http://app.quotemedia.com{HttpContext.Request.Path.ToString().Replace("qm/", "") + HttpContext.Request.QueryString}{appendWebmaster}webmasterId={myConfig.GetValue<string>("Quotemedia:webmasterid")}");
+                return myData;
+            }
+            catch (Exception ex) {
+                return ex.Message;
+            }
+        }
+    }
+    
+}
