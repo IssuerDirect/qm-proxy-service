7ff4b677c9b0b92d847f4d91e48fae50de315da2
Mohammad Nasr <mohammad@net3000.ca>
2021-09-16 12:57:08 -0400

    add snnn_users to dbcontext

---
+++ SNN-api/dbContext/tables/snn_users.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace snn
+{
+    public class snn_users
+    {
+        [Key]
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastname { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string country { get; set; }
+        public int? userid { get; set; }
+        public int? logingroupid { get; set; }
+    }
+}
