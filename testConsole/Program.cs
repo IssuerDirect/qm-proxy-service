c609b23dc86b32a29771ed9a707b6b3881382a4e
Wael Hallag <info@net3000.ca>
2021-09-18 07:44:11 -0400

    added test console application

---
+++ testConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appSettingsFile = $"{AppDomain.CurrentDomain.BaseDirectory}appsettings.json";
+            var builder = new ConfigurationBuilder().AddJsonFile(appSettingsFile, optional: true, reloadOnChange: true).AddEnvironmentVariables();
+            IConfigurationRoot configuration = builder.Build();
+            
+        }
+    }
+}
