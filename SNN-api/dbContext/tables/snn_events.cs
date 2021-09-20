8ee44758ca5e99171b77c88d515bda4c15f2e556
Mohammad Nasr <mohammad@net3000.ca>
2021-09-20 11:27:30 -0400

    create table events

---
+++ SNN-api/dbContext/tables/snn_events.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace snn
+{
+    public class snn_events
+    {
+        [Key]
+        public int id { get; set; }
+        public DateTime? date { get; set; }
+        public String title { get; set; }
+        public String location { get; set; }
+
+    }
+}
