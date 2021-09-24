4d37bc5d6d18e73e0f931e3df9b94fe95097e240
Mohammad Nasr <mohammad@net3000.ca>
2021-09-24 16:26:28 -0400

    filing class

---
+++ SNN-api/dbContext/companyHubTables/cc_SecFiling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace snn
+{
+    public class cc_SecFiling
+    {
+        [Key]
+        public string id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime? create_time { get; set; } = DateTime.Now;
+        public DateTime? update_time { get; set; } = DateTime.Now;
+        public DateTime? occur { get; set; }
+        public string ci_Company { get; set; }
+        public int? ref_Sector { get; set; }
+        public int? ref_FilingType { get; set; }
+        public string accession { get; set; }
+        public string form_type { get; set; }
+        public string title { get; set; }
+        public string src_text { get; set; }
+        public string src_xbrl { get; set; }
+        public string src_xml { get; set; }
+
+    }
+}
