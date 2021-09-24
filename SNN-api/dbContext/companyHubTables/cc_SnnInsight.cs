d5d107d68673fe82aeb031645879e87b07bb7ffb
Wael Hallag <info@net3000.ca>
2021-09-24 16:07:41 -0400

    updated function to save insights, still pending

--- SNN-api/dbContext/companyHubTables/cc_SnnInsight.cs
+++ SNN-api/dbContext/companyHubTables/cc_SnnInsight.cs
@@ -9,7 +9,7 @@ namespace snn
     public class cc_SnnInsight
     {
         [Key]
-        public string id { get; set; } = Guid.NewGuid().ToString();
+        public string id { get; set; }
         public DateTime? create_time { get; set; } = DateTime.Now;
         public DateTime? update_time { get; set; } = DateTime.Now;
         public DateTime? occur { get; set; }
